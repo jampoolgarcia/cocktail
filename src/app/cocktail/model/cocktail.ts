9f56075827e8ff36161ae1e89029845279df28c8
jampool <jampoolgarcia@gmail.com>
2021-06-30 08:20:19 -0400

    added model cocktail

---
+++ src/app/cocktail/model/cocktail.ts
@@ -0,0 +1,73 @@
+export interface CocktailI {
+    id: string;
+    name: string;
+    img: string;
+    glass: string;
+    ingredients: string[];
+    numIngredients: number;
+    measures: string[];
+    numMeasures: number;
+    instruccions: string;
+}
+
+import * as _ from 'lodash';
+
+export class Cocktail implements CocktailI {
+    
+    constructor(data: any){
+        _.set(this, 'data', data);
+    }
+
+    get id(): string{
+        return _.get(this, 'data.idDirnk');
+    }
+
+    get name(): string{
+        return _.get(this, 'data.strDirnk');
+    }
+    
+    get img(): string{
+        return _.get(this, 'data.strDrinkThumb');
+    }
+
+    get glass(): string{
+        return _.get(this, 'data.strGlass');
+    }
+
+    get ingredients(): string[]{
+        return this.getList('strIngredient');
+    }
+
+    get numIngredients(): number{
+        return this.ingredients.length;
+    }
+
+    get measures(): string[]{
+        return this.getList('strMeasure');
+    }
+
+    get numMeasures(): number {
+        return this.measures.length;
+    }
+
+    get instruccions(): string{
+        return _.get(this, 'strInstructionsES') ? _.get(this, 'strInstructionsES') : _.get(this, 'data.strInstrucions');
+    }
+
+    private getList(property: string): string[] {
+
+        let i = 1;
+        let item = _.get(this, `data.${property}${i}`);
+        let list = [];
+
+        while(item){
+            list.push(item);
+            i++;
+            item = _.get(this, `data.${property}${i}`);
+        }
+
+        return list;
+
+    }
+
+}
